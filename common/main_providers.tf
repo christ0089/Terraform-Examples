1403ff12da77b0909abf3bc5ec716ab4516461c6
Christian <christianalva96@gmail.com>
2019-05-31 17:35:41 -0500

    Update to Service

--- common/main_providers.tf
+++ common/main_providers.tf
@@ -14,16 +14,46 @@ provider "aws" {
 }
 
 resource "aws-lambda-functions" "balance_service" {
-  function_name = "User_Balance"
+  function_name = "lambda-3-${substr("${local.api_artifact_name}", 0, min(64, length(local.api_artifact_name)))}"
+
+  runtime = "nodejs8.10"
+  # "index" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "lambda-3/balance.api"
+
+
+  # Optional, but ensures that things don't constantly refresh during local development
+  source_code_hash = "${base64sha256(file("${local.api_dist_dir}"))}"
 }
 
 resource "aws-lambda-functions" "transaction_service" {
-  function_name = "User_Transactions"
+  function_name = "lambda-1-${substr("${local.api_artifact_name}", 0, min(64, length(local.api_artifact_name)))}"
+
+
+  runtime = "nodejs8.10"
+
+ handler = "lambda-3/transaction.api"
+
+  # Optional, but ensures that things don't constantly refresh during local development
+  source_code_hash = "${base64sha256(file("${local.api_dist_dir}"))}"
+
 }
 
 
 resource "aws-lambda-functions" "Card_Manager" {
-  function_name = "Card_Tockenization"
+  function_name = "lambda-2-${substr("${local.api_artifact_name}", 0, min(64, length(local.api_artifact_name)))}"
+
+
+  runtime = "nodejs8.10"
+
+  # "index" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "lambda-2/card_manager.api"
+
+  # Optional, but ensures that things don't constantly refresh during local development
+  source_code_hash = "${base64sha256(file("${local.api_dist_dir}"))}"
 }
 
 
